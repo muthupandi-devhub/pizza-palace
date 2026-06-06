8e32656eb6f5c457c64dcf5e34c32cd8b86d59d2
MUTHU-HUB-DEV <muthupandi030605@gmail.com>
2026-06-07 01:04:14 +0530

    update

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/products";
+const API = "https://pizza-palace-backend-fj5l.onrender.com/api/products";
 
 export const getProducts = async () => {
   const response = await axios.get(API);
